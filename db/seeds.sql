23c625b3e1e7e56dfd297e601bb27a204b612638
jjlarkin <jjlarkin85@gmail.com>
2017-05-16 14:20:36 -0400

    heroku deploy

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
-INSERT INTO burgers (name) VALUES ('Meeses');
-INSERT INTO burgers (name) VALUES ('Bobbi');
-INSERT INTO burgers (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO burgers (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Cheese');
+INSERT INTO burgers (name) VALUES ('Bacon');
+INSERT INTO burgers (name) VALUES ('Veggie');
+INSERT INTO burgers (name, eaten) VALUES ('Ham', true);
+INSERT INTO burgers (name, eaten) VALUES ('Double', true);
+INSERT INTO burgers (name, eaten) VALUES ('Turkey', true);
+
