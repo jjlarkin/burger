9d31c54ec78185495356960167f6c462f946ba48
jjlarkin <jjlarkin85@gmail.com>
2017-05-16 15:02:26 -0400

    testing heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgerController.js");
 var port = 3000;
 
 var app = express();
@@ -15,13 +16,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgersController.js");
+
 
 app.use("/", routes);
 
